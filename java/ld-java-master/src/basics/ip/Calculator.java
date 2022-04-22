7fcacdaff278da7629c6a3632defa29ec8c26702
DEV <dev@dev.com>
2022-04-22 12:06:48 +0530

    commit..

--- java/ld-java-master/src/basics/ip/Calculator.java
+++ java/ld-java-master/src/basics/ip/Calculator.java
@@ -1,7 +1,13 @@
 package basics.ip;
 class Calculator {
+	// overloaded functions
+	// same name / diff type of args
 	public void add(int num1, int num2) {
 		int sum = num1 + num2;
+		System.out.println(sum);
+	}
+	public void add(float num1, int num2) {
+		float sum = num1 + num2;
 		System.out.println(sum);
 	}
 }
