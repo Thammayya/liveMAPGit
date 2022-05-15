fc6d301cb6cbb5d47e8016fa6fcfc9b35c8b20f4
DEV <dev@dev.com>
2022-05-15 16:06:10 +0530

    commit..

--- ARTIFACTS/1 HTML with Javascript/93-promise.js
+++ ARTIFACTS/1 HTML with Javascript/93-promise.js
@@ -3,23 +3,61 @@
 //    // async logic
 // }
 
+
+// DETAILED IMPLEMENTATION
 var taskPromise = new Promise(function (resolveFunction, rejectFunction) {
-  const taskStatus = "completedCleaningRoom";
+//   const taskStatus = "completedCleaningRoom";
+const taskStatus = "playedGames";
   if ((taskStatus == "completedCleaningRoom")) {
-    resolveFunction;
+    resolveFunction();
   } else if (taskStatus != "completedCleaningRoom") {
-    rejectFunction;
+    rejectFunction();
   }
 });
 
 var successFunction = function () {
-   console.log("Promise Resloved as Sunny completed Cleaning the room: ", "Go to play");
+   // console.log("Promise Resloved as Sunny completed Cleaning the room: ", "Go to play");
+   console.log("Go to Play!")
 };
 
 var failedFunction = function () {
-  console.log("Promise Rejected as Sunny did not clean the room and instead played mobile games: ", "Do the laundry");
+//   console.log("Promise Rejected as Sunny did not clean the room and instead played mobile games: ", "Do the laundry");
+console.log("Do the Laundry!");
 };
 
-taskPromise.then(successFunction,failedFunction);
+taskPromise.then(successFunction).catch(failedFunction);
+// for(var i = 0; i< 10000; i++) {
+//    for(var j = 0; j< 1000000; j++) {}  
+// }
+console.log("sync operation complete")
+// SHORT HAND
+// var taskPromise = new Promise(function (resolveFunction, rejectFunction) {
+//   const taskStatus = "completedCleaningRoom";
+//   if ((taskStatus == "completedCleaningRoom")) {
+//     resolveFunction();
+//   } else if (taskStatus != "completedCleaningRoom") {
+//     rejectFunction();
+//   }
+// });
 
 
+
+//SHORT HAND IMPLEMENTATION
+var taskPromise1 = new Promise(function (resolveFunction, rejectFunction) {
+   //   const taskStatus = "completedCleaningRoom";
+   const taskStatus = "completedCleaningRoom";
+     if ((taskStatus == "completedCleaningRoom")) {
+       resolveFunction();
+     } else if (taskStatus != "completedCleaningRoom") {
+       rejectFunction();
+     }
+   });
+
+   taskPromise1.then(()=> console.log("Go to Play!!")).catch(() => console.log("Do the Laundry!!"));
+
+   // SHORTEST
+   new Promise((resolveFunction, rejectFunction) => {
+      //   const taskStatus = "completedCleaningRoom";
+      const taskStatus = "completedCleaningRoom";
+     taskStatus == "completedCleaningRoom"?resolveFunction():rejectFunction()})
+     .then(()=> console.log("Go to Play!!!")).catch(() => console.log("Do the Laundry!!!"));
